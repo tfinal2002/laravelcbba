b5f33c12af31f846c67f5ddb021cea90e1d23aa6
Barrios Juan Jose <info.josebarrios@gmail.com>
2019-07-01 08:57:37 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Cordoba
+                    Laravel Córdoba
                 </div>
 
                 <div class="links">
